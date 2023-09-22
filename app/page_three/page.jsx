03194b11320fb8b50d0365ccd5ccc7a1dcaa75be
rozycos <rozycos@gmail.com>
2023-09-22 23:16:24 +0200

    Created next.js ocllaris app, created and configured pages

---
+++ app/page_three/page.jsx
@@ -0,0 +1,8 @@
+export const metadata = {
+  title: '3 page title',
+  description: '3 page title',
+}
+
+export default function PageThree() {
+  return <h1>Hello, Page Three</h1>
+}
---
+++ app/page_two/page.jsx
@@ -0,0 +1,10 @@
+export const metadata = {
+  title: 'second page title',
+  description: 'second page title',
+}
+
+export default function PageTwo() {
+  return (
+    <h1>Hello, Page Two</h1>
+  )
+}
